77329a7aa87364cc86406dc2180d1db99444acd1
darcipons <darcimarie33@gmail.com>
2019-12-08 13:02:13 -0500

    created form shell

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,11 +2,50 @@ import React from 'react'
 import './Form.css'
 
 class Form extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { 
+      name: '',
+      email: '',
+      repo: '',
+      url: '',
+    }
+
+  
+  }
 
   render() {
     return(
       <div>
-        <h1>Form Page</h1>
+        <form>
+        <input
+            name= 'fullName'
+            placeholder= 'FULL NAME' 
+            value= {this.state.fullName}
+            onChange={e => this.change(e)}
+            autoComplete= "off"
+          />
+          <input
+            name= 'email'
+            placeholder= 'EMAIL' 
+            value= {this.state.email}
+            onChange={e => this.change(e)}
+          />
+          <input
+            name= 'repo'
+            placeholder= 'PROJECT REPO' 
+            value= {this.state.repo}
+            onChange={e => this.change(e)}
+          />
+          <input
+            name= 'url'
+            placeholder= 'PROJECT URL' 
+            value= {this.state.url}
+            onChange={e => this.change(e)}
+          />
+          <button onClick={(e) => this.onSubmit(e)}>Submit</button>
+        </form>
       </div>
     )
   }
