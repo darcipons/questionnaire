32cd7e3fd2e5d8b4b22313fa9e67901f333c862c
darcipons <darcimarie33@gmail.com>
2019-12-08 12:57:18 -0500

    set up form component

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Form.css'
+
+class Form extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <h1>Form Page</h1>
+      </div>
+    )
+  }
+}
+
+
+export default Form 
