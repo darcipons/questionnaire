9f5cb9ad63c574e023941b1b47717ea4b33495f9
darcipons <darcimarie33@gmail.com>
2019-12-09 12:12:34 -0500

    added ratings to email template

--- server.js
+++ server.js
@@ -9,12 +9,27 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.post('/api/form', (req, res) => {
   nodemailer.createTestAccount((err, account) => {
     const htmlEmail = `
-      <h3>Contact Details</h3>
+      <h3>Code Challenge</h3>
       <ul>
         <li>Name: ${req.body.name}</li>
         <li>Email: ${req.body.email}</li>
         <li>Repo: ${req.body.repo}</li>
         <li>URL: ${req.body.url}</li>
+        <li>Using best practices for OOP: ${req.body.rating1}</li>
+        <li>Modular development: ${req.body.rating2}</li>
+        <li>Full-stack workflow understanding: ${req.body.rating3}</li>
+        <li>Testing: ${req.body.rating4}</li>
+        <li>Database knowledge: ${req.body.rating5}</li>
+        <li>Debugging: ${req.body.rating6}</li>
+        <li>Problem solving skills: ${req.body.rating7}</li>
+        <li>Javascript: ${req.body.rating8}</li>
+        <li>HTML: ${req.body.rating9}</li>
+        <li>CSS: ${req.body.rating10}</li>
+        <li>Working on a team: ${req.body.rating11}</li>
+        <li>Self motivation: ${req.body.rating12}</li>
+        <li>Communication skills: ${req.body.rating13}</li>
+        <li>Your own energy level: ${req.body.rating14}</li>
+        <li>Intelligence / Aptitude: ${req.body.rating15}</li>
       </ul>
     `
 
@@ -28,7 +43,7 @@ app.post('/api/form', (req, res) => {
     })
 
     let mailOptions = {
-      from: 'wyncoder33@gmail.com',
+      from: 'darcimarie33@gmail.com',
       to: 'darcimarie33@gmail.com',
       subject: 'Code Challenge',
       html: htmlEmail
