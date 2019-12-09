414b9094d2f5793c7b6d23554773191e19ee202c
darcipons <darcimarie33@gmail.com>
2019-12-09 15:17:02 -0500

    fixing deployment

--- server.js
+++ server.js
@@ -65,12 +65,10 @@ app.listen(PORT, () => {
 })
 
 if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  // Handle React routing, return all requests to React app
+  app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  })
 }
